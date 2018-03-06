cd8b4d38c152b12b587e5559e5dee579bfd1d124
wdarsow <wade.darsow@gmail.com>
2018-03-05 18:03:08 -0600

    folder renamed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// Array for the random psychic letters chosen
+// Array for the random psychic letters chosen 
 const alphabetArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
     'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
